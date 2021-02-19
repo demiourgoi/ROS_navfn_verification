903c610ffde198ebad3fdfa1eb849336299580a0
Rubén Rubio <rubenrub@ucm.es>
2021-02-19 14:24:47 +0100

    Comparison and plotting of potential maps and paths

--- src/maude_planner/compare.py
+++ src/maude_planner/compare.py
@@ -5,10 +5,12 @@ Check navigation results from ROS2 and Maude implementation
 Author: Enrique Martin
 """
 
+import argparse
 import json
+import math
 import sys
 
-EPSILON = 1E-6
+EPSILON = 1E-5
 
 
 def file_to_dict(filename):
@@ -54,15 +56,140 @@ def path_equal(path1, path2):
     while pos < len(path1) and equal_epsilon(path1[pos], path2[pos]):
         pos += 1
     return pos == len(path1)
-    
-  
-  
-def main(file1, file2):
-    """Compares the results stored in file1 and file2, showing the result in the standard output.
-       I assume file1 are the results from ROS and file2 those from Maude   
+
+
+class Plotter:
+    """Plotter of potentials and paths using matplotlib"""
+
+    def __init__(self, name, draw, width):
+        self.width = width
+        
+        # This may be a dummy plotter if draw is false. The packages are only
+        # imported if drawing is required.
+        if draw:
+            try:
+                import numpy as np
+                import matplotlib.pyplot as plt
+                from matplotlib.patches import Rectangle
+                from matplotlib.backends.backend_pdf import PdfPages
+
+            except ImportError as ie:
+                print(f'Some required packages for plotting are missing: {ie}')
+                draw = False
+
+        # We want and we can draw
+        if draw:
+            self.np = np
+            self.plt = plt
+            self.Rectangle = Rectangle
+            self.pdf = PdfPages(name)
+
+        else:
+            self.np = None
+            self.plt = None
+            self.Rectangle = None
+            self.pdf = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        if self.pdf is not None:
+            self.pdf.close()
+
+    def draw_potential(self, potarr, path, pcolor='red'):
+        """Draw the potential and path produced by a backend"""
+
+        # The maximum value not being the pseudo-infinity
+        # (the minimum should always be zero)
+        vmax = potarr[self.np.abs(potarr) < 1e5].max()
+
+        # Draw the potential as a image omitting infinity
+        # (otherwise, the other differences could not be seen)
+        self.plt.imshow(potarr, vmax=vmax)
+        self.plt.colorbar()
+
+        # Draw red squares in the infinity
+        ax = self.plt.gca()
+
+        for (x, y), value in self.np.ndenumerate(potarr):
+            if abs(value) > 1e5:
+               ax.add_patch(self.Rectangle([y-.5, x-.5], 1, 1, edgecolor='red', facecolor='red'))
+
+        # Finally, the path
+        self.plt.plot(*zip(*path), color=pcolor)
+
+    def draw_diff(self, potarr1, potarr2, path1, path2):
+        """Draw the difference of the given potentials and their paths"""
+
+        diff = potarr1 - potarr2
+        support = self.np.abs(diff) < 1e5
+
+        # The maximum and minimum value where infinity is not present
+        # (in order to see differences in the output for smaller changes)
+        vmax = diff[support].max()
+        vmin = diff[support].min()
+
+        self.plt.imshow(diff, vmin=vmin, vmax=vmax)
+        self.plt.colorbar()
+
+        self.plt.plot(*zip(*path1), color='aqua')
+        self.plt.plot(*zip(*path2), color='red')
+
+        # Draw red (green) squares in the infinity values of Maude (ROS)
+        ax = self.plt.gca()
+
+        for (x, y), value in self.np.ndenumerate(diff):
+            if abs(value) > 1e5:
+                color = 'red' if value < 0.0 else 'aqua'
+                ax.add_patch(self.Rectangle([y-.5, x-.5], 1, 1, edgecolor=color))
+
+    def draw(self, origin, dest, potarr1_raw, potarr2_raw, path1, path2, equal):
+        """Plots potentials, paths and their differences"""
+
+        # If we are not drawing or do not have enough information
+        if self.np is None or potarr2_raw is None:
+            return
+
+        # Calculate the width and height of the map
+        # (it should common for all cases though)
+        width = int(self.np.sqrt(len(potarr1_raw))) if self.width is None else self.width
+        height = len(potarr1_raw) // width
+
+        # Convert the potentials to NumPy arrays
+        potarr1 = self.np.array(potarr1_raw)
+        potarr2 = self.np.array(potarr2_raw)
+        potarr1.shape = (width, height)
+        potarr2.shape = (width, height)
+
+        # Maude's potentials are transposed
+        potarr2 = self.np.transpose(potarr2)
+
+        # ROS potential and path
+        self.draw_potential(potarr1, path1, pcolor='aqua')
+        self.plt.title(f'{origin} to {dest} — ROS')
+        self.pdf.savefig()
+        self.plt.close()
+
+        # Maude potential and path
+        self.draw_potential(potarr2, path2, pcolor='red')
+        self.plt.title(f'{origin} to {dest} — Maude')
+        self.pdf.savefig()
+        self.plt.close()
+
+        # Difference and paths
+        self.draw_diff(potarr1, potarr2, path1, path2)
+        self.plt.title(f'{origin} to {dest} {"☺" if equal else "☹"}')
+        self.pdf.savefig()
+        self.plt.close()
+
+
+def main(args):
+    """Compares the results stored in args.ros_output and args.maude_output, showing the result in the standard output.
+       Writes the potentials to a PDF if args.draw is enabled.
     """
-    dict1 = file_to_dict(file1)
-    dict2 = file_to_dict(file2)
+    dict1 = file_to_dict(args.ros_output)
+    dict2 = file_to_dict(args.maude_output)
     key_set1 = set(dict1.keys())
     key_set2 = set(dict1.keys())
     
@@ -71,19 +198,37 @@ def main(file1, file2):
         print(f"{file1} - {file2}: {key_set1 - key_set2}")
         print(f"{file2} - {file1}: {key_set2 - key_set1}")
         tests = sorted(list(key_set1.intersect(key_set2)))
-        
+
     num_diff = 0
-    for test in tests:
-        # Remove duplicates in the goal position
-        path1 = remove_last(dict1[test]["path"])
-        path2 = remove_last(dict2[test]["path"])
-        if not path_equal(path1, path2):
-            num_diff += 1
-            print(f'{num_diff}) Differences in the path from {test[0]} to {test[1]}')
-            print(f'ROS  : {path1}')
-            print(f'Maude: {path2}')
-            print(f'potarr: {dict1[test]["navfn"]}\n')
-        
-            
+    with Plotter('potentials.pdf', args.draw, args.width) as plotter:
+        for test in tests:
+            # Remove duplicates in the goal position
+            # TODO Rubén: remove_last is no longer neededs
+            path1 = remove_last(dict1[test]["path"])
+            path2 = remove_last(dict2[test]["path"])
+
+            potarr1 = dict1[test]["navfn"]
+            potarr2 = dict2[test].get("navfn")
+
+            equal = path_equal(path1, path2)
+
+            if not equal:
+                num_diff += 1
+                print(f'{num_diff}) Differences in the path from {test[0]} to {test[1]}')
+                print(f'ROS  : {path1}')
+                print(f'Maude: {path2}')
+                print(f'potarr: {dict1[test]["navfn"]}\n')
+
+            # Draw the potentials and paths
+            plotter.draw(test[0], test[1], potarr1, potarr2, path1, path2, equal)
+
+
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    parser = argparse.ArgumentParser(description='Compare ROS and Maude results')
+    parser.add_argument('ros_output', help='JSON test results produced by profile_cpp')
+    parser.add_argument('maude_output', help='JSON test results produced by profile_maude')
+    parser.add_argument('--draw', help='Draw navigation functions and paths (requires matplotlib)', action='store_true')
+    parser.add_argument('--width', '-w', help='Width of the map (if not square)', type=int)
+
+    args = parser.parse_args()
+    main(args)
