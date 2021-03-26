7dfe306f5aa35e99d9bf43d954ad6068c5f07da9
Enrique Martín <emartinm@ucm.es>
2021-03-26 10:14:50 +0100

    Reproducible PDF in test-one

--- src/maude_planner/test_one.sh
+++ src/maude_planner/test_one.sh
@@ -8,6 +8,11 @@ DIRNAME=$(dirname $test)
 ROS_OUT="$DIRNAME/ros.txt"
 MAUDE_OUT="$DIRNAME/maude.txt"
 
+# Make the generated PDF reproducible by omitting the creation date
+# (the Matplotlib version may also be omitted)
+# (https://matplotlib.org/2.1.1/users/whats_new.html#reproducible-ps-pdf-and-svg-output)
+export SOURCE_DATE_EPOCH="0"
+
 ./profile_cpp $test > $ROS_OUT
 python profile_maude.py $test > $MAUDE_OUT
 python compare.py $ROS_OUT $MAUDE_OUT
